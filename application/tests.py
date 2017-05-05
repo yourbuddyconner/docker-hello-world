eb43496f399c751be23f74fd01ec5624a85d9a85
Conner Swann <conner.swann@hiqlabs.com>
2017-05-05 13:22:29 -0700

    tests

--- application/tests.py
+++ application/tests.py
@@ -8,8 +8,8 @@ class AppTestCase(unittest.TestCase):
    def test_root_text(self):
         tester = app.test_client(self)
         response = tester.get('/')
-        assert 'Hello world!' in response.data
-        #assert 'Buttz' in response.data
+        #ssert 'Hello world!' in response.data
+        assert 'Buttz' in response.data
 
 if __name__ == '__main__':
     unittest.main()
